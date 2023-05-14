88274af53b8d6374e76e0cdc2db0eab3423afb24
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-05-14 21:49:53 +0900

    Add dijkstar.py for compute path
    Signed-off-by: cobang0111 <97373900+cobang0111@users.noreply.github.com>

---
+++ dijkstra.py
@@ -0,0 +1,217 @@
+import heapq
+import numpy as np
+global INF
+INF = 2550
+
+#Function : Dijkstra - Calculate distance of all point from start point
+def dijkstra(maze, start):
+    n = len(maze) #Row of maze
+    m = len(maze[0]) #Column of maze
+    dist = np.array([[INF] * m for _ in range(n)]) #Fill the maze with INF
+    dist[start[0]][start[1]] = 0 #Set the start point as 0
+    heap = [(0, start)] #Initial heap which has start point and initial cost
+    
+    #Until heap element exists
+    while heap:
+        cost, pos = heapq.heappop(heap) #Pop the element from heap
+        # Case : The smaller value of cost already exists
+        if dist[pos[0]][pos[1]] < cost:
+            continue
+        
+        #N, NE, E, SE, S, SW, W, NW -> 8 direction Searching from current position
+        for dy, dx in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]:
+            #Next position
+            next_y, next_x = pos[0] + dy, pos[1] + dx
+            # Case : Index out of range
+            if next_y < 0 or next_y >= n or next_x < 0 or next_x >= m:
+                continue
+            # Case : Obstacle
+            if maze[next_y][next_x] == -1:
+                continue
+            # Cost of next position
+            if (dy == 0 or dx == 0):
+                next_cost = cost + 10 
+            else : 
+                next_cost = cost + 14
+            # Case : Update cost of next position to minimum cost
+            if next_cost < dist[next_y][next_x]:
+                dist[next_y][next_x] = next_cost
+                #Update heap
+                heapq.heappush(heap, (next_cost, (next_y, next_x)))
+    return dist
+
+#Function : Find shortest path from start point to target point
+def find_shortest_path(maze, start, target):
+    dist = dijkstra(maze, start)
+    n = len(maze) #Row of maze
+    m = len(maze[0]) #Column of maze
+    path = [(target[0], target[1])] #stack of path
+    #Until start point
+    while path[-1] != start:
+        y, x = path[-1]
+        min_dist = dist[y][x] #Current point
+        for dy, dx in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]:
+            next_y, next_x = y + dy, x + dx
+            # Case : Index out of range
+            if next_y < 0 or next_y >= n or next_x < 0 or next_x >= m:
+                continue
+            # Case : Current point > Precede point -> update
+            if dist[next_y][next_x] < min_dist:
+                min_dist = dist[next_y][next_x]
+                next_pos = (next_y, next_x)
+        path.append(next_pos)
+    return path
+
+#Function : Make can and PET position index list from given map
+def find_pet_can_list(given_map):
+    pet_list = []
+    can_list = []
+    for i in range(len(given_map)):
+        for j in range(len(given_map[0])):
+            if given_map[i][j][0] == 'p':
+                pet_list.append((i, j))
+            elif given_map[i][j][0] == 'c':
+                can_list.append((i, j))
+    return pet_list, can_list
+
+#Function : Make map which has shortest path from PET and Can to trash area
+def find_shortest_classfy_path(maze, final_map, pet_list, can_list, start):
+    #PET
+    for i, j in pet_list:
+        dist = dijkstra(maze, (i, j))
+        min_pet = INF
+        #Find minimum distance from PET to trash area
+        for k in range(len(maze)):
+            if dist[k][0] < min_pet:
+                min_pet = dist[k][0]
+        final_map[[i],[j]] = min_pet 
+    #CAN
+    for i, j in can_list:
+        dist = dijkstra(maze, (i, j))
+        min_can = INF
+        #Find minimum distance from can to trash area
+        for k in range(len(maze)):
+            if dist[k][len(maze[0])-1] < min_can:
+                min_can = dist[k][len(maze[0])-1]
+        final_map[[i],[j]] = min_can
+    print(final_map)
+    #Approach
+    for i, j in pet_list+can_list:
+        #Refactoring needed for efficiency
+        temp_maze = maze
+        temp_maze[i][j] = 0
+        dist = dijkstra(temp_maze, start)
+        final_map[[i],[j]] += dist[[i],[j]] 
+    print(final_map)  
+    return final_map
+
+#Function : Find most efficient index from final map
+def find_most_efficient_index(map):
+    min = INF
+    min_index = (0, 0)
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] != 0 and map[i][j] < min:
+                min = map[i][j]
+                min_index = (i, j)
+    return min_index
+
+#Function : Classify trash using efficient path
+def classify_trash(given_map, start):
+    maze = np.zeros((len(given_map),len(given_map[0])))
+    final_map = np.zeros((len(given_map),len(given_map[0])))
+    
+    pet_list, can_list = find_pet_can_list(given_map)
+        
+    for i, j in pet_list+can_list:
+        maze[[i],[j]] = -1
+    final_map = find_shortest_classfy_path(maze, final_map, pet_list, can_list, start)
+    print('\n')
+    print(final_map)
+
+    target = find_most_efficient_index(final_map)
+    dist = dijkstra(maze, target)
+    final_path = []
+
+    if given_map[target[0]][target[1]][0] == 'p':
+        min_pet = INF
+        min_pet_row = 0
+        for i in range(len(maze)):
+            if dist[i][0] < min_pet:
+                min_pet = dist[i][0]        
+                min_pet_row = i
+        final_path = find_shortest_path(maze, target, (min_pet_row, 0))
+    else :
+        min_can = INF
+        min_can_row = 0
+        for i in range(len(maze)):
+            if dist[i][len(maze[0])-1] < min_can:
+                min_can = dist[i][len(maze[0])-1]        
+                min_can_row = i
+        final_path = find_shortest_path(maze, target, (min_can_row, len(maze[0])-1))
+        
+    temp_path = find_shortest_path(maze, start, target)[1:]
+    final_path += temp_path
+    start = final_path[0]   
+    return start, final_path
+    
+
+#Main              
+given_map = [[('x', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('y', 0)],
+             [('x', 0), ('e', 0), ('p', 1), ('c', 2), ('e', 0), ('p', 3), ('y', 0)],
+             [('x', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('y', 0)],
+             [('x', 0), ('p', 4), ('e', 0), ('c', 5), ('c', 6), ('e', 0), ('y', 0)],
+             [('x', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('e', 0), ('y', 0)]]
+
+start = (3,6)
+print("START AT :", start)
+start, path = classify_trash(given_map, start)
+
+print("RESULT : ")
+print(start)
+print(path)
+
+move_order = []
+
+row_1, col_1 = path.pop()
+while(path):
+    row_2, col_2 = path.pop()
+    dy = row_2 - row_1
+    dx = col_2 - col_1
+    #N
+    if dy < 0 and dx == 0:
+        new = [0, 5, 0, 0]
+    #NE
+    elif dy < 0 and dx > 0:
+        new = [0, 5, 1, 5]
+    #E
+    elif dy == 0 and dx > 0:
+        new = [0, 0, 1, 5]
+    #SE
+    elif dy > 0 and dx > 0:
+        new = [1, 5, 1, 5]
+    #S
+    elif dy > 0 and dx == 0:
+        new = [1, 5, 0, 0]
+    #SW
+    elif dy > 0 and dx < 0:
+        new = [1, 5, 0, 5]
+    #W
+    elif dy == 0 and dx < 0:
+        new = [0, 0, 0, 5]
+    #NW
+    else :
+        new = [0, 5, 0, 5]
+    
+    if (move_order):
+        old = move_order[-1]    
+        if (old[0]==new[0] and old[2] == new[2]):
+            move_order[-1][1] += new[1]
+            move_order[-1][3] += new[3]
+        else:
+            move_order.append(new)
+    else :
+        move_order.append(new)
+    row_1, col_1 = row_2, col_2
+
+print(move_order)
