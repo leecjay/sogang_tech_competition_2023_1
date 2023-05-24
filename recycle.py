eecdad6814fce823ece80537f6f8681f69f2d948
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-05-24 16:17:02 +0900

    Update recycle.py
    Signed-off-by: cobang0111 <97373900+cobang0111@users.noreply.github.com>

--- recycle.py
+++ recycle.py
@@ -10,70 +10,48 @@ import dijkstra
 #time.sleep(1)
 
 #Serial Setup
-py_serial = serial.Serial(port = '/dev/ttyUSB1', baudrate = 9600)
+py_serial = serial.Serial(port = '/dev/ttyUSB0', baudrate = 9600)
 
-#Open webCam
-webCam = cv.VideoCapture(0)
-if not webCam.isOpened():
-    print("Can't open camera")	
-    exit()
-    
+
+#Camera Setup
+#webCam.set(cv.CAP_PROP_FRAME_WIDTH, 1000) #width = 2560px
+#webCam.set(cv.CAP_PROP_FRAME_HEIGHT, 720) #height = 1440px
+picture = "fswebcam --no-banner --set brightness=60% Images/test1.jpg"
+os.system(picture)
+img = cv.imread("Images/test1.jpg", cv.IMREAD_COLOR)
+resize_img = cv.resize(img, (1020,720), interpolation=cv.INTER_AREA)
+cv.imwrite("Images/test1.jpg", resize_img)
+
+#Image Analysis
+yolo = "python3 /home/sgme/yolov5/detect.py > /home/sgme/yolov5/output.txt --weights /home/sgme/yolov5/best.pt --img 640 --conf 0.4 --source /home/sgme/Images/test1.jpg"
+os.system(yolo)
+time.sleep(1)
+
+#object list
+lines = open('/home/sgme/yolov5/output.txt').readlines()
+given_map=file_read.map()
 start_row = 0
 start_col = 0
-
-#Take picture
-usable, img = webCam.read()
-if not usable:
-    print("Can't recieve img")
-else:
-    resize_img = cv.resize(img, (1020,720), interpolation=cv.INTER_CUBIC)
-    
-    while True:
-	    #Save Image
-	    cv.imshow("webCam", resize_img)
-	    cv.imwrite("/home/sgme/Images/test1.jpg", resize_img)
-	    #Image Analysis
-	    yolo = "python3 /home/sgme/yolov5/detect.py > /home/sgme/yolov5/output.txt --weights /home/sgme/yolov5/best.pt --img 640 --conf 0.3 --source /home/sgme/Images/test1.jpg"
-	    os.system(yolo)
-	    time.sleep(1)
-	    #Check object
-	    lines = open('/home/sgme/yolov5/output.txt').readlines()
-	    #Case : Zero object
-	    if len(lines)<=1:
-		    print("There is no remain object!")
-		    break
-	    #Case : Object exist
-	    else:	
-		    #Make Map using object coordination
-		    given_map=file_read.map()
-		    #print map
-		    for i in range(len(given_map)) :
-			    print(given_map[i])
-		    #Make move order from start position
-	    
-		    move_order, end_row, end_col = dijkstra.main(given_map, start_row, start_col)
-		    
-		    print(move_order)
-		    
-		    
-		    
-		    #msg = "go"
-		    #py_serial.write(msg.encode())
-	    
-		    while len(move_order):
-			    py_serial.write(move_order.pop(0))
-			    print("pop")
-			    time.sleep(2) #delay time decided by velocity and distance
+while True :
+	for i in range(len(given_map)) :
+		print(given_map[i])
+	#Make move order from start position
+	move_order, given_map, start_row, start_col, pet_list, can_list = dijkstra.main(given_map, start_row, start_col)
+		#msg = "go"
+		#py_serial.write(msg.encode())
+		#time.sleep(2)
+	while len(move_order):
+		py_serial.write(move_order.pop(0))
+		print("pop")
+		time.sleep(2) #delay time decided by velocity and distance
+		#return_msg = py_serial.readline().decode('utf-8')
+		#print(return_msg)
+		#while(return_msg[0:4] != "done"):
+			#return_msg = py_serial.readline().decode('utf-8')
+			#print(return_msg)
+			#time.sleep(0.5)
+	if (len(pet_list) == 0 and len(can_list) == 0):
+		print("FINISH\n")
+		break
+	time.sleep(8)
 		
-		    return_msg = py_serial.readline().decode('utf-8')
-		    print(return_msg)
-		    
-		    while(return_msg[0:4] != "done"):
-			    print(return_msg)
-			    time.sleep(1)
-		    time.sleep(5)
-		    start_row = end_row
-		    start_col = end_col
-	    
-webCam.release()
-cv.destroyAllWindows()
