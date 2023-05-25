b11915759bc381c23c2dac6deff63868be98c842
cobang0111 <97373900+cobang0111@users.noreply.github.com>
2023-05-25 12:27:36 +0900

    Update file_read.py
    Signed-off-by: cobang0111 <97373900+cobang0111@users.noreply.github.com>

--- file_read.py
+++ file_read.py
@@ -18,21 +18,21 @@ def map():
         object_list.append(temp_list) #last object
         f.close
 
-        #Make result_map to find efficient route(640x480, 12x16 matrix)
-        rows = 12
+        #Make result_map to find efficient route
+        rows = 28
         cols = 16
         result_map = [[('e',0) for j in range(cols)] for i in range(rows)]
 
-        for m in range(0,rows):
-            y_m = m*(480/rows) + (480/(2*rows))
+        for m in range(0,(rows-4):
+            y_m = m*(720/(rows-4)) + (720/(2*(rows-4)))
             for n in range(0,cols):
-                x_m = n*(640/cols) + (640/(2*cols))
+                x_m = n*(1020/cols) + (1020/(2*cols))
                 for k in range(len(object_list)):
                     if (x_m > object_list[k][0]) and (x_m < object_list[k][2]) and (y_m > object_list[k][1]) and (y_m < object_list[k][3]):
                         if object_list[k][4] == 1:
-                            result_map[m][n] = ('p',k+1)
+                            result_map[m+2][n] = ('p',k+1)
                             break
                         elif object_list[k][4] == 0:
-                            result_map[m][n] = ('c',k+1)
+                            result_map[m+2][n] = ('c',k+1)
                             break
         return result_map    
